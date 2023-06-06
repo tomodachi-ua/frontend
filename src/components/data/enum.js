830d93cba898be857468a450b65579ab5b54ea32
disiqui <ditriksy@gmail.com>
2023-06-06 18:23:22 +0300

    Add json with data

---
+++ src/components/data/enum.js
@@ -0,0 +1,16 @@
+export const Type = {
+    MANGA: 'Манґа',
+    MANHWA: 'Манхва',
+    ONESHOT: 'Ваншот',
+    WEBMANHWA: 'Вебманхва',
+};
+
+export const Status = {
+    PUBLISHED: 'Видано',
+    ONGOING: 'Видається'
+}
+
+export const TranslationStatus = {
+    IN_PROGRESS: 'Триває',
+    DONE: 'Перекладено'
+}
