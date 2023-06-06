d20f00f1264eada318062e6a1244b39a7e2bb4be
disiqui <ditriksy@gmail.com>
2023-06-06 18:12:15 +0300

    Add manga list

--- src/components/MangaItem.jsx
+++ src/components/MangaItem.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import styles from "../styles/manga_item.module.scss";
 import {Link} from "react-router-dom";
+import {Type} from "./data/enum"
 
 const MangaItem = (props) => {
-    const { name, cover, genres, type, chaptersCount, translatedChaptersCount, id} = props;
+    const {name, cover, genres, type, chaptersCount, translatedChaptersCount, id} = props;
+    const [mangaType, setMangaType] = useState("");
+    useEffect(() => {
+        switch (type) {
+            case "MANGA":
+                setMangaType(Type.MANGA);
+                break;
+            case "MANHWA":
+                setMangaType(Type.MANHWA);
+                break;
+            case "ONESHOT":
+                setMangaType(Type.ONESHOT);
+                break;
+            case "WEBMANHWA":
+                setMangaType(Type.WEBMANHWA);
+                break;
+        }
+    }, [])
+
+
     return (
         <Link to={`/manga/${id}`} className={styles.manga_item}>
             <div className={styles.img_wrap}>
@@ -17,7 +37,7 @@ const MangaItem = (props) => {
                 <p className={styles.genres}>{genres.map(genre => (
                     <span className={styles.genre}>{genre}</span>
                 ))}</p>
-                <p className={styles.type}>{type}</p>
+                <p className={styles.type}>{mangaType}</p>
             </div>
         </Link>
     );
