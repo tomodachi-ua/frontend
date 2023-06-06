233508a6f8399bf49772eead379d5c1e5d29a08e
disiqui <ditriksy@gmail.com>
2023-06-06 18:03:43 +0300

    Change background, add a new item to Route

--- src/App.js
+++ src/App.js
@@ -6,17 +6,20 @@ import About from "./pages/About";
 import "./styles/common.scss"
 import MangaList from "./pages/MangaList";
 import MangaPage from "./pages/MangaPage";
+import JoinPage from "./pages/JoinPage";
+
 function App() {
     return (
         <BrowserRouter>
-          <Routes>
-            <Route element={<Layout/>}>
-              <Route path="/" element={<About/>}/>
-              <Route path="/manga" element={<MangaList/>}/>
-              <Route path="/manga/:id" element={<MangaPage/>}/>
-            </Route>
-            {/*<Route path='*' element={<Error404Page/>}/>*/}
-          </Routes>
+            <Routes>
+                <Route element={<Layout/>}>
+                    <Route path="/" element={<About/>}/>
+                    <Route path="/manga" element={<MangaList/>}/>
+                    <Route path="/manga/:id" element={<MangaPage/>}/>
+                    <Route path="/join" element={<JoinPage/>}/>
+                </Route>
+                {/*<Route path='*' element={<Error404Page/>}/>*/}
+            </Routes>
         </BrowserRouter>
     );
 }
