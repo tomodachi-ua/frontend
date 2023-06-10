f1b561b27beb6caa8a851be8184d6906df9f14ee
disiqui <ditriksy@gmail.com>
2023-06-10 16:13:27 +0300

    Add header mobile version

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,13 @@ const Header = () => {
                     <img src={logo} alt="TOMODACHI" className={styles.logo}/>
                 </Link>
 
-                <nav>
+                <button className={styles.burger}>
+                    <svg width="26" height="18" viewBox="0 0 26 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M1 9H25M1 1H25M1 17H25" stroke="#161616" stroke-width="1.5" stroke-linecap="round"
+                              stroke-linejoin="round"/>
+                    </svg>
+                </button>
+                <nav className={styles.nav}>
                     <ul className={styles.nav_list}>
                         <li className={styles.nav_list_item}>
                             <Link to="/" className={styles.nav_list_link}>
