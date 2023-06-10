2fc54539692ba577e0241f2efe75c5efc78522e8
disiqui <ditriksy@gmail.com>
2023-06-10 14:34:53 +0300

    Add getAllMangas request

--- src/Logic/requests.js
+++ src/Logic/requests.js
@@ -3,3 +3,9 @@ import axios from "axios";
 export const getManga = async (manga_id) => {
     return await axios.get(`http://localhost:8080/api/v1/manga/${manga_id}`);
 }
+
+export const getAllMangas = async () => {
+    return await axios.get(`http://localhost:8080/api/v1/manga/`);
+}
+
+
