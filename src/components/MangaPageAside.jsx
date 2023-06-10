1690a0ed414a000428c62c4d916e09269b1206db
disiqui <ditriksy@gmail.com>
2023-06-10 14:38:09 +0300

    Implement the request and display of a concrete manga

--- src/components/MangaPageAside.jsx
+++ src/components/MangaPageAside.jsx
@@ -6,12 +6,11 @@ import {TranslationStatus} from "./data/enum";
 import {Status} from "./data/enum";
 
 const MangaPageAside = (props) => {
-    // const [loading, setLoading] = useOutletContext();
     const {data} = props;
 
     return (
         <aside className={styles.aside}>
-            <img src={data.cover} alt="Cover not found" className={styles.cover}/>
+            <img src={data.coverUrl} alt="Cover not found" className={styles.cover}/>
             <MangaInfoItem title={"Автор"} content={data.author} titleStyle={styles.list_title} textStyle={styles.text}/>
             <MangaInfoItem title={"Статус"} content={data.status === "PUBLISHED" ? Status.PUBLISHED : Status.ONGOING} titleStyle={styles.list_title} textStyle={styles.text}/>
             <MangaInfoItem title={"Статус перекладу"} content={data.translationStatus === "IN_PROGRESS" ? TranslationStatus.IN_PROGRESS : TranslationStatus.DONE} titleStyle={styles.list_title} textStyle={styles.text}/>
--- src/pages/MangaPage.jsx
+++ src/pages/MangaPage.jsx
@@ -2,34 +2,24 @@ import React from "react";
 import styles from "../styles/manga_page.module.scss";
 import {useEffect, useState} from "react";
 import {useOutletContext, useParams} from "react-router-dom";
-import data from "../components/data/data.json";
 import MangaPageMain from "../components/MangaPageMain";
 import MangaPageAside from "../components/MangaPageAside";
-// import {getManga} from "../Logic/requests";
+import {getManga} from "../Logic/requests";
 
 const MangaPage = () => {
     const [loading, setLoading] = useOutletContext();
     const {id} = useParams();
     const [mangaInfo, setMangaInfo] = useState({});
 
-    // useEffect(() => {
-    //     setLoading(true);
-    //     getManga(id).then(manga => {
-    //         // setMangaInfo(manga);
-    //         console.log("3232323");
-    //     }).catch(error => {
-    //         console.log(error);
-    //     })
-    //     setLoading(false);
-    // }, [])
     useEffect(() => {
-        data.forEach(element => {
-            if (+element.id === +id) {
-                setMangaInfo(element);
-            }
-        });
-    }, [id]);
-
+        setLoading(true);
+        getManga(id).then(response => {
+            setMangaInfo(response.data);
+        }).catch(error => {
+            console.log(error);
+        })
+        setLoading(false);
+    }, [])
 
     return (
         <main>
