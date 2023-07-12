e1cab4555f3c0c1686d09d2dd7d6f8c38ac03c8d
disiqui <ditriksy@gmail.com>
2023-07-12 19:00:24 +0300

    Change logic of checking and displaying what type of data has been received

--- src/components/MangaInfoItem.jsx
+++ src/components/MangaInfoItem.jsx
@@ -1,29 +1,25 @@
 import React from "react";
 
 const MangaInfoItem = (props) => {
-    if (Array.isArray(props.content)) {
-        return (
-            <div>
-                <h6 className={props.titleStyle}>
-                    {props.title}
-                </h6>
-                <div className={props.listStyle}>
-                    {props.content.map(value => (
-                        <p key={value} className={props.itemStyle}>{value}</p>
-                    ))}
-                </div>
+    let content;
+
+    if (!Array.isArray(props.content)) {
+        content = [props.content];
+    } else content = props.content;
+
+    return (
+        <div>
+            <h6 className={props.titleStyle}>
+                {props.title}
+            </h6>
+            <div className={props.listStyle}>
+                {content.map(value => (
+                    <p key={value} className={props.itemStyle}>{value}</p>
+                ))}
             </div>
-        )
-    } else {
-        return (
-            <div className={props.infoStyle}>
-                <h6 className={props.titleStyle}>
-                    {props.title}
-                </h6>
-                <p className={props.textStyle}>{props.content}</p>
-            </div>
-        )
-    }
+        </div>
+    )
+
 }
 
 export default MangaInfoItem;
