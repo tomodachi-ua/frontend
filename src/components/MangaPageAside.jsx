6659344ffa240caa19044f93294fdb7cfadffd0a
disiqui <ditriksy@gmail.com>
2023-07-12 18:58:00 +0300

    Change position of components on the page

--- src/components/MangaPageAside.jsx
+++ src/components/MangaPageAside.jsx
@@ -10,9 +10,32 @@ const MangaPageAside = (props) => {
     return (
         <aside className={styles.aside}>
             <img src={data.coverUrl} alt="Cover not found" className={styles.cover}/>
-            <MangaInfoItem title={"Автор"} content={data.author} titleStyle={styles.list_title} textStyle={styles.text} infoStyle={styles.infoStyle}/>
-            <MangaInfoItem title={"Статус"} content={data.status === "PUBLISHED" ? Status.PUBLISHED : Status.ONGOING} titleStyle={styles.list_title} textStyle={styles.text} infoStyle={styles.infoStyle}/>
-            <MangaInfoItem title={"Статус перекладу"} content={data.translationStatus === "IN_PROGRESS" ? TranslationStatus.IN_PROGRESS : TranslationStatus.DONE} titleStyle={styles.list_title} textStyle={styles.text} infoStyle={styles.infoStyle}/>
+
+            <h2 className={styles.title}>{data.name}</h2>
+
+            <MangaInfoItem content={data.alternativeNames}
+                           listStyle={styles.list}
+                           titleStyle={styles.list_title}
+                           itemStyle={styles.alt_names_item}/>
+
+            <MangaInfoItem content={data.genres}
+                           listStyle={styles.list}
+                           itemStyle={styles.item}
+                           titleStyle={styles.list_title}/>
+
+            <MangaInfoItem title={"Автор"} content={data.author}
+                           titleStyle={styles.list_title}
+                           textStyle={styles.text}/>
+
+            <MangaInfoItem title={"Статус"}
+                           content={data.status === "PUBLISHED" ? Status.PUBLISHED : Status.ONGOING}
+                           titleStyle={styles.list_title}
+                           textStyle={styles.text} />
+
+            <MangaInfoItem title={"Статус перекладу"}
+                           content={data.translationStatus === "IN_PROGRESS" ? TranslationStatus.IN_PROGRESS : TranslationStatus.DONE}
+                           titleStyle={styles.list_title}
+                           textStyle={styles.text}/>
         </aside>
     )
 }
--- src/components/MangaPageMain.jsx
+++ src/components/MangaPageMain.jsx
@@ -4,14 +4,14 @@ import MangaInfoItem from "./MangaInfoItem";
 import MangaProgressBar from "./MangaProgressBar";
 
 const MangaPageMain = (props) => {
-    const { data } = props;
+    const {data} = props;
     return (
         <section className={styles.main_content}>
-            <h2 className={styles.title}>{data.name}</h2>
-            <MangaInfoItem content={data.alternativeNames} listStyle={styles.list} titleStyle={styles.list_title} itemStyle={styles.alt_names_item}/>
-            <MangaInfoItem content={data.genres} listStyle={styles.list} itemStyle={styles.item} titleStyle={styles.list_title}/>
-            <MangaInfoItem title={"Опис"} content={data.description} titleStyle={styles.list_title} textStyle={styles.text}/>
-            <MangaProgressBar current={data.translatedChaptersCount} total={data.chaptersCount}/>
+            <MangaInfoItem title={"Опис"} content={data.description}
+                           titleStyle={styles.list_title}
+                           textStyle={styles.text}/>
+            <MangaProgressBar current={data.translatedChaptersCount}
+                              total={data.chaptersCount}/>
         </section>
     )
 }
